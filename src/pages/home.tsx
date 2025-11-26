8a8cfd31cc5655b4d9975735757d9135b21a7f2f
millan-figueroa <millan.fig@gmail.com>
2025-11-25 18:15:59 -0800

    Creates page.tsx, styles the bottom half grey for the background effect, and imports the components

---
+++ src/pages/home.tsx
@@ -0,0 +1,20 @@
+import Hero from "../components/Hero";
+import URLShortenerForm from "../components/URLShortenerForm";
+import StatsSection from "../components/StatsSection";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <URLShortenerForm />
+
+      {/* Grey background section */}
+      <div className="bg-gray-100">
+        <StatsSection />
+        {/* Later: shortened links, boost CTA */}
+        <Footer />
+      </div>
+    </>
+  );
+}
